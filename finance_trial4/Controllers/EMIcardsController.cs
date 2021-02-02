39ee2877c5f0bd74a252b1988a5e01e8e9b0ee2d
akhil5672 <akhilkumar5672@gmail.com>
2021-02-02 11:46:30 +0530

    api part

---
+++ finance_trial4/Controllers/EMIcardsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using finance_trial4.Models;
+
+namespace finance_trial4.Controllers
+{
+
+    public class EMIcardsController : ApiController
+    {
+        private financedbEntities6 db = new financedbEntities6();
+        Customer tempcustomer = new Customer();
+       // [Route("api/EMIinsert")]
+        public IHttpActionResult PostEMIinsertion(Customer tempcustomer)
+        {
+            
+            EMIcard temp = db.EMIcards.Where(x => x.customer_id == tempcustomer.customer_id).FirstOrDefault();
+            if (temp != null)
+            {
+                return Ok(0);
+            }
+            else
+            {
+                System.DateTime date = DateTime.Now;
+                EMIcard emicard = new EMIcard();
+                var tempCustomerDetails = db.Customers.ToList();
+                CustomerCard a = new CustomerCard();
+                a = (from ab in tempCustomerDetails
+                     join ct in db.CardTypeMasters on ab.cardtype_id equals ct.EMIcardtype_id
+                     select new CustomerCard
+                     {
+                         customer_id = ab.customer_id,
+                         total_limit = ct.total_limit,
+                         EMIcard_validity = ct.EMIcard_validity
+                     }
+                       ).Where(x => x.customer_id == tempcustomer.customer_id).SingleOrDefault();
+                emicard.customer_id = a.customer_id;
+                emicard.EMIcard_expiry = date.AddYears(a.EMIcard_validity);
+                emicard.used_credit = 0;
+                emicard.remaining_credit = a.total_limit;
+                //int num = random.Next(1000);
+                //Math.Floor((Math.random() * 100) + 1);
+
+                Random random = new Random();
+                emicard.EMIcard_number = random.Next(1000000000, int.MaxValue);
+                db.EMIcards.Add(emicard);
+                db.SaveChanges();
+
+            }
+            return Ok(1);
+        }
+        // GET: api/EMIcards
+        public IQueryable<EMIcard> GetEMIcards()
+        {
+            return db.EMIcards;
+        }
+
+        // GET: api/EMIcards/5
+        [ResponseType(typeof(EMIcard))]
+        public IHttpActionResult GetEMIcard(int id)
+        {
+            EMIcard eMIcard = db.EMIcards.Find(id);
+            if (eMIcard == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(eMIcard);
+        }
+
+        // PUT: api/EMIcards/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEMIcard(int id, EMIcard eMIcard)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != eMIcard.EMIcard_id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(eMIcard).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EMIcardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/EMIcards
+        [ResponseType(typeof(EMIcard))]
+        //public IHttpActionResult PostEMIcard(EMIcard eMIcard)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    db.EMIcards.Add(eMIcard);
+        //    db.SaveChanges();
+
+        //    return CreatedAtRoute("DefaultApi", new { id = eMIcard.EMIcard_id }, eMIcard);
+        //}
+
+        // DELETE: api/EMIcards/5
+        [ResponseType(typeof(EMIcard))]
+        public IHttpActionResult DeleteEMIcard(int id)
+        {
+            EMIcard eMIcard = db.EMIcards.Find(id);
+            if (eMIcard == null)
+            {
+                return NotFound();
+            }
+
+            db.EMIcards.Remove(eMIcard);
+            db.SaveChanges();
+
+            return Ok(eMIcard);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EMIcardExists(int id)
+        {
+            return db.EMIcards.Count(e => e.EMIcard_id == id) > 0;
+        }
+    }
+}
---
+++ finance_trial4/Controllers/ordersController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using finance_trial4.Models;
+
+namespace finance_trial4.Controllers
+{
+    public class ordersController : ApiController
+    {
+
+        private financedbEntities6 db = new financedbEntities6();
+
+        public IHttpActionResult PostOrders(orderattributes orderattributes)
+        {
+            productsMaster product = db.productsMasters.Where(x => x.product_id == orderattributes.product_id).FirstOrDefault();
+            order order = new order();
+            EMItypeMaster EMItype = db.EMItypeMasters.Where(x => x.EMItype_id == orderattributes.EMItype_id).FirstOrDefault();
+            EMIcard emicard = db.EMIcards.Where(x => x.customer_id == orderattributes.customer_id).FirstOrDefault();
+            if(emicard==null)
+            {
+                return Ok(-1);
+            }
+            
+            if(emicard.remaining_credit<product.product_price)
+            {
+                return Ok(0);
+            }
+            else
+            {
+                order.product_id = orderattributes.product_id;
+                order.customer_id = orderattributes.customer_id;
+                order.EMItype_id = orderattributes.EMItype_id;
+                order.EMI_amount = product.product_price / EMItype.EMI_tenure;
+                order.order_date = DateTime.Now;
+                order.order_status = false;
+                db.orders.Add(order);
+                db.SaveChanges();
+                emicard.remaining_credit = emicard.remaining_credit - product.product_price;
+                emicard.used_credit = emicard.used_credit + product.product_price;
+                db.Entry(emicard).State = EntityState.Modified;
+                db.SaveChanges();
+                return Ok(1);
+               
+            }
+           
+        }
+        // GET: api/orders
+        public IQueryable<order> Getorders()
+        {
+            return db.orders;
+        }
+
+        // GET: api/orders/5
+        [ResponseType(typeof(order))]
+        public IHttpActionResult Getorder(int id)
+        {
+            order order = db.orders.Find(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(order);
+        }
+
+        // PUT: api/orders/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putorder(int id, order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != order.order_id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!orderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/orders
+        [ResponseType(typeof(order))]
+        //public IHttpActionResult Postorder(order order)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    db.orders.Add(order);
+        //    db.SaveChanges();
+
+        //    return CreatedAtRoute("DefaultApi", new { id = order.order_id }, order);
+        //}
+
+
+        // DELETE: api/orders/5
+        [ResponseType(typeof(order))]
+        public IHttpActionResult Deleteorder(int id)
+        {
+            order order = db.orders.Find(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            db.orders.Remove(order);
+            db.SaveChanges();
+
+            return Ok(order);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool orderExists(int id)
+        {
+            return db.orders.Count(e => e.order_id == id) > 0;
+        }
+    }
+}
